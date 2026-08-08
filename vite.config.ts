2e64450e44fc314aa3c04793e9224d3cd2600849
PRINCE0774 <princek0774@gmail.com>
2026-08-08 11:28:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/90-days-in-1lakh-practical-course/',
+  base: '/practical-course/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
